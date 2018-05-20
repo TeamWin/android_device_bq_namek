52c7d756a7ae1d53ed127d12c9dd3501695b569f
Kra1o5 <kra1o5x@gmail.com>
2018-05-20 23:40:18 +0200

    Update to N
    Change-Id: I2e78adec05c21cf5bfa16cb7abd399cf49bf7cb7

--- omni_namek.mk
+++ omni_namek.mk
@@ -1,9 +1,20 @@
-# Inherit device configuration
-$(call inherit-product, device/bq/namek/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/bq/namek/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata \
+    device/bq/namek/kernel:kernel
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := namek
 PRODUCT_NAME := omni_namek
 PRODUCT_BRAND := bq
-PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris M5.5
+PRODUCT_MANUFACTURER := bq
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=Aquaris_M55 PRODUCT_NAME=Aquaris_M55
